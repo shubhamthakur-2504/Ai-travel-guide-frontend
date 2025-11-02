3baa28f3216b2adf191ee0d10e1a6fe396098d3d
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-11-03 01:53:55 +0530

    fix: fixed build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import Home from "./pages/home";
-import Plan from "./pages/plan";
+import Home from "./pages/Home";
+import Plan from "./pages/Plan";
 import Result from "./pages/Result";
 function App() {
 
